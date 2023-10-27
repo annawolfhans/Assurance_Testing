0367c7b8bc2c03bf4cdad787212b4cd72aa9f756
Anna Wolford <17awolford@gmail.com>
2023-10-27 10:06:37 -0600

    CLEANED UP AND ADDED COSTEXTRACT FUNCTION

--- ReformatWorkspace.R
+++ ReformatWorkspace.R
@@ -1,163 +1,4 @@
-wordExtract <- function(string){
-  middle=stringr::str_sub(stringr::str_extract_all(string,
-                                                   "\\*([^*]+)\\*"),2,-2)
-  middleWords<-unlist(strsplit(middle, ','))
-  endWord=stringr::str_sub(stringr::str_extract(string, "\\*([^*]+)\\*"), 2, -1)
-  return(c(middleWords, endWord))
-}
-
-file <- textConnection("S(*TIRES*:cost=$4:*BackTire*:prior=beta(1,2):test=TRUE:cost=$1,
-                       *FrontTire*:prior=beta(3,2):test=TRUE:cost=$1)")
-
-# If you specify a cost, then it is testable. If not, then it is not testable, cost=Inf
-# Somehow designate that x item has already been defined somewhere else, therefore not giving prior
-## So for tires, you wouldn't give it a prior because it's comprised of BackTire and FrontTire
-# Prior(a=1,b=2) and if it doesn't have a prior, find from previous components 
-# textConnection() is like finding a file. The user could create/find a file instead, but it's easier to keep contained in one file
-# Put in vector, and use positions
-
-## Think about how to explain this to someone, how to go about it
-# Spaces allowed? Character name? 
-# Build in warnings as well for all of those :)
-
-
-############## REWRITE WITH THE FOLLOWING EXAMPLE CODE ###########
-file <- textConnection("S(*TIRES*:$2.00:,
-	        *BackTire*:B(1,2):TRUE:$1:, 
-          *FrontTire*:B(1,2):TRUE:$1:), 
-
-          P(*BRAKES*:$55:
-	        *BackBrake*:B(1,2):TRUE:$1:, 
-          *FrontBrake*:B(1,2):TRUE:$1:), 
-
-          S(*BIKE*:$55:
-          *TIRES*:, 
-          *BRAKES*:)")
-
-text=suppressWarnings(readLines(file))
-text <- paste(text, collapse = "")
-text <- gsub("\\s+", "", text)
-# file <- textConnection("S(*BackTire*:prior=B(1,2):test=TRUE:cost=$1,000.52:, *FrontTire*:prior=B(1,2):test=TRUE:cost=$1:prior=B(2,3)), *Tires*")
-# text=suppressWarnings(readLines(file))
-
-
-# REWRITTEN TO SUPPORT EACH COMPONENT BEING SURROUNDED BY ASTERISKS
-# Pulls them because they are surrounded by asterisks, then 
-wordExtract <- function(string){
-  pattern <- "\\*([^*]+)\\*"
-  matches <- stringr::str_extract_all(text, pattern)
-  result_string <-""
-  for (match_list in matches) {
-    for (match in match_list) {
-     result_string <- paste(result_string, match)
-    }
-  }
-  cleaned_string <- gsub("\\*", "", result_string)
-  cleaned_string <- stringr::str_trim(cleaned_string)
-  stringr::str_replace_all(cleaned_string, " ", "")
-  words <- unlist(strsplit(cleaned_string, ' '))
-  if (length(words) > 1) {
-    endWord <- words[length(words)]
-    middleWords <- words[1:(length(words) - 1)]
-  } else {
-    endWord <- ""
-    middleWords <- words
-  }
-  return(c(middleWords, endWord))
-}
-
-words <- wordExtract(text)
-
-needsBuilt<-stringr::str_sub(stringr::str_extract(text, ":[a-zA-Z0-9]+"), 2, -1)
-
-if (length(needsBuilt)!=length(unique(needsBuilt)))stop("Non-unique definitions given to component (check right side of colon for non-unique values)")
-## Anything is 'ready' if it is not on the RHS of a definition
-ready<-setdiff(words, needsBuilt)
-
-## Check to make sure all components only appear once on the LHS of the relations (enforce that data not used twice)
-counts<-table(words)
-countIndices<-names(counts)%in% names(table(needsBuilt))
-if(any(counts[countIndices]>2) | !all(counts[!countIndices]==1))
-  stop("Component names must be unique on LHS of equations")
-
-
-
-############################ EXTRACT TEST COST ########################
-testCostExtract <- function(string) {
-  pattern <- "\\$[0-9.,]+(?=[:])"
-  result <- stringr::str_extract_all(string, pattern)[[1]]  
-  return(result)
-}
-
-testCostExtract(text)
-
-################## EXTRACT PRIORS ################
-## Based on B( , )
-priorsExtract <- function(string) {
-  pattern <- "B\\((\\d+),(\\d+)\\)"
-  matches <- stringr::str_match_all(string, pattern)
-  if (length(matches) > 0) {
-    extracted_numbers <- matches[[1]][, 2:3]
-    return(extracted_numbers)
-  } else {
-    return(NULL)
-  }
-}
-
-priors <- priorsExtract(text)
-colnames(priors) <- c("alpha", "beta")
-priors
-## turn this into a df, then name the rows as well...
-
-# Should I keep it as a matrix, or switch to a vector of numbers?
-
-priors <- as.integer(priorsExtract(text))
-alpha <- numeric(0)
-beta <- numeric(0)
-
-for (i in seq_along(priors)) {
-  if (i %% 2 == 1) {
-    alpha <- c(alpha, priors[i])
-  } else {
-    beta <- c(beta, priors[i])
-  }
-}
-
-made_samp <- FALSE
-samp=NULL
-mcsamps=1000
-if(length(samp) == 0){
-  samp <- list()
-  for(i in 1:length(alpha)){
-    samp[[i]] <- rbeta(mcsamps, alpha[i], beta[i])
-  }
-  made_samp <- TRUE
-}
-
-# this outputs samp[[1 through 4]]. I need a better way to associate them 
-
-# running into problem from before - how should I use this moving forward. 
-# I could perform the sampling and store into their respective variable names? 
-
- 
- 
-#### TF EXTRACT: SHOULDN"T NEED THIS CODE ANY MORE ####
-# trueFalseTestExtract <- function(string) {
-#   pattern <- c("TRUE","FALSE", "true", "false", "True", "False")
-#   matches <- stringr::str_extract_all(string, pattern)
-#   if (length(matches) > 0) {
-#     extracted_words <- unlist(matches)
-#     return(extracted_words)
-#   } else {
-#     return(NULL)
-#   }
-# }
-# trueFalseTestExtract(text)
- 
-#####################
-
-# Rewrite WordExtract code
-# Create a connection to the text data
+#### EXAMPLE FILE ####
 file <- textConnection(
 "BIKE::PARALLEL
 Tires: cost=$33
@@ -176,34 +17,34 @@ Power::parallel
 Pedaling: B(1,2), cost=$12
 Electric: B(1,2), cost=$Inf")
 
+#### ACCEPT EITHER TEXTCONNECTION() OR .TXT FILE ####
 # Read the lines from the connection 
 # IF use textConnection great. If not, make it work
 #### come back to this - problem is that it splits it into separate strings ####
-# process_text_data <- function(data_input) {
-#   if (inherits(data_input, "textConnection")) {
-#     text <- readLines(data_input)
-#   } else if (file.exists(data_input)) {
-#     text <- readLines(data_input)
-#   } else {
-#     stop("Input must be a textConnection object or a valid file path.")
-#   }
-#   
-#   # Process the text data here or perform any desired operations
-#   # For example, you can print the lines of text:
-#   # cat(text_data, sep = "\n")
-# }
-# 
-# process_text_data(file)
+file_path <- path.expand("~/file.txt")
+file_path <- as.character(file_path)  # Ensure it's a character string
 
-#### continue ####
+processInput <- function(input_source) {
+  if (inherits(input_source, "textConnection")) {
+    input_string <- paste(readLines(input_source), collapse = " ")
+  } else if (file.exists(input_source)) {
+    input_string <- paste(readLines(input_source), collapse = " ")
+  } else {
+    stop("Invalid input source. Please provide a valid textConnection or file path.")
+  }
+  print(input_string)
+}
+
+processInput(file_path)
+
+#### BEGIN WARNINGS/PULLING INFORMATION #### 
+
 text <- suppressWarnings(readLines(file))
 if(length(text)==0)stop("Text file contained 0 elements")
 
 ## Checks to see if includes parallel or series relationship after::
 # CASE INSENSITIVE
 # pattern says it can't have a space - maybe specify in text or build a warning for it? 
-
-
 for (sentence in text) {
   if (stringr::str_count(sentence, "::") == 1) {
     if (!grepl("(?i)::(parallel|series)", sentence)) {
@@ -270,7 +111,7 @@ for (i in 2:line_count) {
   }
 }
 
-
+#### EXTRACT PRIORS ####
 ## Based on B( , )
 priorsExtract <- function(string) {
   pattern <- "B\\((\\d+),(\\d+)\\)"
@@ -286,6 +127,23 @@ text <- paste(text, collapse = " ")
 priors <- priorsExtract(text)
 colnames(priors) <- c("alpha", "beta")
 priors
+
+
+#### EXTRACT COST ####
+costExtract <- function(string) {
+  pattern <- "\\(\\d+,\\d+\\), cost=\\$(\\d+|Inf)"
+  matches <- stringr::str_match_all(string, pattern)
+  
+  if (length(matches) > 0) {
+    extracted_numbers <- as.numeric(matches[[1]][, 2])
+    return(extracted_numbers)
+  } else {
+    return(NULL)
+  }
+}
+text <- paste(text, collapse = " ")
+costs <- costExtract(text)
+costs
 
 ## Check to make sure all components only appear once on the LHS of the relations (enforce that data not used twice)
 counts<-table(words)
